8243c526a0ed59074f70b84dbbd917ce5d198f94
Justin Garza <jgarza9788@gmail.com>
2022-09-30 12:32:26 -0400

    20220930

---
+++ makeReadMe.py
@@ -0,0 +1,16 @@
+# remakes the readme file
+
+import os
+
+DIR = os.path.dirname(os.path.abspath(__file__))
+
+output = []
+
+for  file in os.listdir(DIR):
+    # print(file)
+    if file.endswith('.md') and file != 'ReadMe.md':
+        output.append('[{0}]({0}) \n'.format(file))
+    
+with open(os.path.join(DIR, 'ReadMe.md'),'w') as f:
+    f.writelines(output)
+
