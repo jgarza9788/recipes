10381f1406d07d3133b53aaab0bcc153d4a71ca7
jgarza9788 <jgarza9788@gmail.com>
2022-11-24 18:08:29 -0500

    20221124

--- makeReadMe.py
+++ makeReadMe.py
@@ -7,9 +7,10 @@ DIR = os.path.dirname(os.path.abspath(__file__))
 output = []
 
 for  file in os.listdir(DIR):
-    # print(file)
     if file.endswith('.md') and file != 'ReadMe.md':
+        print(file)
         output.append('[{0}]({0})  \n'.format(file))
+    
     
 with open(os.path.join(DIR, 'ReadMe.md'),'w') as f:
     f.writelines(output)
